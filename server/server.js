4796b41cafa7d3889104cb80115d5722f3b5d052
Bryan L. Venable <bryanlvenable@gmail.com>
2015-02-13 10:35:08 -0800

    added basic requirements to server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-//inject configuration to server here here
+// server.js
+
+// Require all tools we will need
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+
