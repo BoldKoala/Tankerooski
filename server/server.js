6d5e96a8a4ed63ac30062968d5da1493ea33d943
Bryan L. Venable <bryanlvenable@gmail.com>
2015-02-13 11:02:48 -0800

    configured server.js

--- server/server.js
+++ server/server.js
@@ -7,3 +7,31 @@ var port = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
 
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var configDB = require('./config/db.js');
+
+// configureation
+mongoose.connect(configDB.url);
+
+// set up our express application
+app.use(morgan('dev'));   // log eveery request to the console
+app.use(cookieParser());  // read cookies (needed for auth)
+app.use(bodyParser());    // get information from html forms
+
+app.set('view engine', 'ejs');
+
+// required for passport
+app.use(session({ secret: 'ilovelouiseslouislouislouis'})); // session secret
+app.use(passport.initialize());
+app.use(passport.session());  // persisstent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+require('./config/router.js')(app, passport);  // load our routes and pass in our app and fully configured passport
+
+// launch
+app.listen(port);
+console.log('I hear there is a party on port ' + port + '...');
